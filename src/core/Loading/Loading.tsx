43fed6b3fea2571ad19ff4d594931fb9749698e0
Scott Williams <contact@scottw.io>
2017-11-24 20:38:19 +0000

    loading animation

---
+++ src/core/Loading/Loading.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import './Loading.css';
+ 
+const Loading = (props) => (
+  <div className="Pacman-wrapper">
+    <div className="Pacman">
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  </div>
+);
+
+export {Loading};
