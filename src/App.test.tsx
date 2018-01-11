f9b366917be4d955d65d52a4313d6bca1384d2cb
Scott Williams <contact@scottw.io>
2018-01-11 17:58:53 +0000

    Cleaning up router adding app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,23 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import { MemoryRouter } from 'react-router';
+import { Provider } from 'react-redux' ;
+import configureStore from 'redux-mock-store';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App  />, div);
+  let state = {menu: {open: false}};
+  const mockStore = configureStore();
+  let store = mockStore(state);
+  let menu = {open: false};
+
+  ReactDOM.render(
+  
+    <MemoryRouter>
+      <Provider store={store}>
+        <App menu={menu}/>
+      </Provider>
+    </MemoryRouter>
+    , div);
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import * as React from 'react';
 import './App.css';
 import './Normalize.css';
-import { Route, withRouter, Router } from 'react-router-dom';
+import { Route, withRouter, Switch } from 'react-router-dom';
 import { HomeContainer } from './pages/home/HomeContainer';
 import { MenuContainer } from './core/Menu/MenuContainer';
 import { ProjectsContainer } from './pages/projects/ProjectsContainer';
 import { ProjectsDetailsContainer } from './pages/projectDetails/ProjectsDetailsContainer';
-import { Switch } from 'react-router';
 import { AboutContainer } from './pages/about/AboutContainer';
 import { toggleMenu } from './store/menu/menuActions';
-import createHistory from 'history/createBrowserHistory';
-
-const history = createHistory();
 
 class App extends React.Component {
   props: any;
@@ -20,9 +16,7 @@ class App extends React.Component {
   }
   render() {
     return (
-      <Router history={history}>
-       <AppWithRouter menu={this.props.menu} onCloseMenu={() => { this.closeMenu(); }} />
-      </Router>
+        <AppWithRouter menu={this.props.menu} onCloseMenu={() => { this.closeMenu(); }} />
     );
   }
 }
