731920c584974bcdfd369fc0cbfc08df9be72fda
Scott Williams <contact@scottw.io>
2018-01-11 21:24:44 +0000

    export const rather than functions

--- src/store/Projects/projectsReducer.tsx
+++ src/store/Projects/projectsReducer.tsx
@@ -2,13 +2,11 @@ import { ProjectsModel } from './projectsModel';
 import { ActionTypes } from '../actionsTypes';
 import { projectsActions } from './projectsActions';
 
-function projectsReducer(state: ProjectsModel = new ProjectsModel(), action: ActionTypes) {
+export const projectsReducer = (state: ProjectsModel = new ProjectsModel(), action: ActionTypes)  => {
   switch (action.type) {
     case projectsActions.GET_PROJECTS_SUCCESS:
       return Object.assign({}, action.payload);
     default:
       return state;
   }
-}
-
-export {projectsReducer};
+};
